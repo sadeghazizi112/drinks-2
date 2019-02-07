33a274dfe982215b1abb8f55f4f11447bdc2198f
sadegh azizi <sadeghazizi112@yahoo.com>
2019-02-07 10:54:53 -0800

    second half of this

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
-  var age = parseInt(prompt("How old are you?"));
+  var over21 = confirm("Are you over 21? Click OK for yes or Cancel for no.");
 
-  if (age >= 21) {
+  if (over21) {
     $('#drinks').show();
+  } else {
+    $('#under-21').show();
   }
 });
